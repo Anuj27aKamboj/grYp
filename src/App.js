e71f950af083eb978d4711ce4b32a9662b4810e5
Anuj Kamboj <anujkamboj.27@Anujs-MacBook-Air.local>
2025-10-29 13:16:52 +0530

    Added Error Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { HeaderComponent } from "./components/HeaderComponent";
 import { BodyComponent } from "./components/BodyComponent";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import ErrorComponent from "./components/ErrorComponent";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const AppLayout = () => {
   return (
@@ -12,5 +16,21 @@ const AppLayout = () => {
   );
 };    
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element:<AppLayout />,
+    errorElement: <ErrorComponent />
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path:"/contact",
+    element: <Contact />,
+  }
+]
+)
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
---
+++ src/components/ErrorComponent.js
@@ -0,0 +1,7 @@
+const ErrorComponent =()=>{
+    return <div>
+        <img src="public/error_component_image.jpg" title="error icons" />
+    </div>
+}
+
+export default ErrorComponent
